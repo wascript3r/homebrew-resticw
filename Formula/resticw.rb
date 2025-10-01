5f2004e53599697a5fff92d49b797ab414f9e24c
WaScript3r <wascript3r@gmail.com>
2025-10-01 19:13:59 +0300

    update

--- Formula/resticw.rb
+++ Formula/resticw.rb
@@ -1,25 +1,29 @@
 class Resticw < Formula
+  version "0.0.3"
+
   on_macos do
     on_intel do
-      url "https://github.com/wascript3r/homebrew-resticw/releases/download/v0.0.2/resticw-darwin-amd64.tar.gz"
-      sha256 "dad9f97499ba9dc3033c1b6b899d62f242c643b66693eb1a8550d653a023e4bf"
+      url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-amd64.tar.gz"
+      sha256 "9e57d0f465491b23dad8abd56ea9c1f40b1e34ba3279a65ac7d3417c15d30ebb"
     end
     on_arm do
-      url "https://github.com/wascript3r/homebrew-resticw/releases/download/v0.0.2/resticw-darwin-arm64.tar.gz"
-      sha256 "66afe5d4ee5d0da99766a4bb60c38ba76066992bd0f0dbc11870060f61ec673f"
+      url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-arm64.tar.gz"
+      sha256 "7897b04a6f5967f50b4ef4f5c77e958abf5a4c7c1d716cd06c454e06f4f52378"
     end
   end
 
   def install
     bin.install "resticw"
+    (etc/"resticw").mkpath
+    (var/"resticw").mkpath
   end
 
   service do
-    run [opt_bin/"resticw"]
+    run opt_bin/"resticw"
     keep_alive true
+    restart_delay 60
+    working_dir var/"resticw"
+    error_log_path var/"log/resticw-error.log"
     environment_variables PATH: "/usr/local/bin:#{std_service_path_env}"
-    # working_dir var/"resticw"
-    # log_path var/"log/myservice.log"
-    # error_log_path var/"log/myservice-error.log"
   end
 end
