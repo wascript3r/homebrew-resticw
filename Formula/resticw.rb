ff6d3eec7321bcdd89baa16a23fc54231900971c
WaScript3r <wascript3r@gmail.com>
2025-10-01 20:09:20 +0300

    update

--- Formula/resticw.rb
+++ Formula/resticw.rb
@@ -1,14 +1,14 @@
 class Resticw < Formula
-  version "0.0.3"
+  version "0.0.4"
 
   on_macos do
     on_intel do
       url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-amd64.tar.gz"
-      sha256 "9e57d0f465491b23dad8abd56ea9c1f40b1e34ba3279a65ac7d3417c15d30ebc"
+      sha256 "97d047799bac6ce755e97cb1f2e9a74b7890bf5ba03c43e21d65340dcb1f1c04"
     end
     on_arm do
       url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-arm64.tar.gz"
-      sha256 "7897b04a6f5967f50b4ef4f5c77e958abf5a4c7c1d716cd06c454e06f4f52378"
+      sha256 "89ef73fabdd2bf3291b4d21d0aa2ed8417ad008d1365130f4c0e862410a55eec"
     end
   end
 
@@ -19,7 +19,7 @@ class Resticw < Formula
   end
 
   service do
-    run opt_bin/"resticw"
+    run [opt_bin/"resticw", "-c", etc/"resticw/config.json", "-s", var/"resticw/state.json"]
     keep_alive true
     restart_delay 60
     working_dir var/"resticw"
