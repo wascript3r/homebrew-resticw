c2b907dfb164a89b2e092dfbd9c509389e924172
WaScript3r <wascript3r@gmail.com>
2025-10-02 20:59:40 +0300

    update

--- Formula/resticw.rb
+++ Formula/resticw.rb
@@ -1,14 +1,14 @@
 class Resticw < Formula
-  version "0.0.5"
+  version "0.0.6"
 
   on_macos do
     on_intel do
       url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-amd64.tar.gz"
-      sha256 "23f93ca820ddf9dc47be3c98d8338a9b04ae963cdaff1c7729e9c746d1b59cd7"
+      sha256 "6e78c0b0472e3c81303a1eca068500fa93d838d85390c0efeed26e2316ed15a4"
     end
     on_arm do
       url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-arm64.tar.gz"
-      sha256 "6a9aeb0b6674874c315eb277e579b446bd01298b1a46127b35ec0be333d458e6"
+      sha256 "369cb677ec851412bff3fe692217a82f5332561896aeaf6abe35c530ae8de06c"
     end
   end
 
