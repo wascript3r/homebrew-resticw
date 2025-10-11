316bf9e94160bf2e9e2c79f80d103f2a22c2df7b
WaScript3r <wascript3r@gmail.com>
2025-10-11 16:25:00 +0300

    update

--- Formula/resticw.rb
+++ Formula/resticw.rb
@@ -1,29 +1,29 @@
-class Resticw < Formula
+class Wbackup < Formula
   version "0.0.12"
 
   on_macos do
     on_intel do
-      url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-amd64.tar.gz"
+      url "https://github.com/wascript3r/homebrew-wbackup/releases/download/v#{version}/wbackup-darwin-amd64.tar.gz"
       sha256 "967fdf7b1ad4eca3f8a7a24a71e04dadbdc65a683d88fb7de8b3ae2f7d7a4dce"
     end
     on_arm do
-      url "https://github.com/wascript3r/homebrew-resticw/releases/download/v#{version}/resticw-darwin-arm64.tar.gz"
+      url "https://github.com/wascript3r/homebrew-wbackup/releases/download/v#{version}/wbackup-darwin-arm64.tar.gz"
       sha256 "7be5c10db47e487382661ecfe2502a3e05e0511d43940bb38bb10effb90e0d0c"
     end
   end
 
   def install
-    bin.install "resticw"
-    (etc/"resticw").mkpath
-    (var/"resticw").mkpath
+    bin.install "wbackup"
+    (etc/"wbackup").mkpath
+    (var/"wbackup").mkpath
   end
 
   service do
-    run [opt_bin/"resticw", "-c", etc/"resticw/config.json", "-s", var/"resticw/state.json"]
+    run [opt_bin/"wbackup", "-c", etc/"wbackup/config.json", "-s", var/"wbackup/state.json"]
     keep_alive true
     restart_delay 60
-    working_dir var/"resticw"
-    error_log_path var/"log/resticw-error.log"
+    working_dir var/"wbackup"
+    error_log_path var/"log/wbackup.log"
     environment_variables PATH: "/usr/local/bin:#{std_service_path_env}"
   end
 end
