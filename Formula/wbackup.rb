eb76922f1bfb155382bdca7402479eef7677cb78
WaScript3r <wascript3r@gmail.com>
2025-10-11 20:40:47 +0300

    update

--- Formula/wbackup.rb
+++ Formula/wbackup.rb
@@ -1,14 +1,14 @@
 class Wbackup < Formula
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     on_intel do
       url "https://github.com/wascript3r/homebrew-wbackup/releases/download/v#{version}/wbackup-darwin-amd64.tar.gz"
-      sha256 "2658d5a12999645757f05b2371ef5a64d8792da3a01054304008ae8fc3a999df"
+      sha256 "a738fb16c59ca1dc878759e3be315b6513df4ed1f88a78184eddb61744ec10d3"
     end
     on_arm do
       url "https://github.com/wascript3r/homebrew-wbackup/releases/download/v#{version}/wbackup-darwin-arm64.tar.gz"
-      sha256 "2ca9a7ebba40dfefebe109c851fd80d10ffccdc1aa2e5f140a1747a793d62fab"
+      sha256 "87d47b8d657ec61055508faf9f2f296eeb60a76c12f5ecd987120b5026d8547d"
     end
   end
 
